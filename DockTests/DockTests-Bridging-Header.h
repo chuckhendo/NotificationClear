0b8c44792934e30a6c66a0dea1342a41f2977722
Carl Henderson <chuckhendo@gmail.com>
2015-02-24 08:13:07 -0500

    Changed version to 0.1; set correct class

--- DockTests/DockTests-Bridging-Header.h
+++
@@ -1,7 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#import <RSSwizzle.h>
-#import "MySamplePlugin.h"
-#import "NCNotificationTableController.h"
